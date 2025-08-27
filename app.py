878458c39ddc765c444573f0a0419b06d1c0f36c
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-08-27 22:23:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  
 os.environ["OMP_NUM_THREADS"] = "8"  
 os.environ["TF_FORCE_CPU_ONLY"] = "1"  
-import tensorflow as tf 
+import tensorflow as tf  
 from model_utils import focal_loss_fixed, MelanomaRecall, NevusSpecificity, CombinedMetric, ThresholdOptimizer 
 import streamlit as st 
 
