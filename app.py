6b0a9c02995c199be3240f5914645190d0abc64f
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-08-28 00:43:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image, ImageOps 
 import os 
 import base64   
-os.environ["CUDA_VISIBLE_DEVICES"] = "" 
+os.environ["CUDA_VISIBLE_DEVICES"] = ""  
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"   
 os.environ["OMP_NUM_THREADS"] = "8"  
 os.environ["TF_FORCE_CPU_ONLY"] = "1"  
