4d67437a0f0622e04dbbde63952002526242584b
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-08-28 16:20:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -249,11 +249,11 @@ elif st.session_state.screen == "Result":
     if 'result' in st.session_state:
         result, prob, color = st.session_state.result
         st.image(st.session_state.image, caption="", use_container_width=True)
-        st.markdown(f'<div style="background-color: {color}; color: white; padding: 10px; border-radius: 5px; text-align: center;">{result}</div>', unsafe_allow_html=True)
-        if result == "Benign":
+        st.markdown(f'<div style="background-color: {color}; color: white; padding: 10px; border-radius: 5px; text-align: center;">Result: {result}</div>', unsafe_allow_html=True)
+        if result == "probably benign mole":
             st.markdown(f'<div class="normal-text">This should be a benign mole. </div>', unsafe_allow_html=True)
             st.markdown(f'<div class="normal-text">Yet, if it is asymmetrical, has an irregular border, several colors, a diameter >6mm and/or has evolved recently, show it to a dermatologist.</div>', unsafe_allow_html=True)
-        else:
+        elif result == "possible melanoma":
             st.markdown(f'<div class="normal-text">This could be a melanoma, meaning a cluster of cancerous cells.</div>', unsafe_allow_html=True)
             st.markdown(f'<div class="normal-text">No need to worry, melanomas are highly treatable if detected early. Show it to a dermatologist in 2-4 weeks.</div>', unsafe_allow_html=True)
 
