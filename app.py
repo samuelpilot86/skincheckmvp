b551f7be762749db87cd0f8ebcb9ff70a52e5f5d
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-08-28 11:32:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,46 +7,15 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["OMP_NUM_THREADS"] = "8"  
 os.environ["TF_FORCE_CPU_ONLY"] = "1" 
 import tensorflow as tf
-from model_utils import focal_loss_fixed, MelanomaRecall, NevusSpecificity, CombinedMetric, ThresholdOptimizer
+from model_utils import focal_loss_fixed, MelanomaRecall, NevusSpecificity, CombinedMetric, ThresholdOptimizer, load_model, preprocess_image, predict_user_image
 import streamlit as st
 from st_clickable_images import clickable_images
 from streamlit_cropper import st_cropper
 
 # Charger le modèle
 @st.cache_resource
-def load_model():
-    custom_objects = {
-        'focal_loss_fixed': focal_loss_fixed(gamma=1.0, alpha=0.9),
-        'MelanomaRecall': MelanomaRecall,
-        'NevusSpecificity': NevusSpecificity,
-        'CombinedMetric': CombinedMetric,
-        'ThresholdOptimizer': ThresholdOptimizer
-    }
-    return tf.keras.models.load_model('skin_lesion_model_binary.keras', custom_objects=custom_objects)
 model = load_model()
-# Fonction de prétraitement
-def preprocess_image(image, target_size=(224, 224)):
-    try:
-        img = image.convert('RGB')
-        img = img.resize(target_size, Image.Resampling.LANCZOS)
-        img_array = np.array(img) / 255.0
-        return img_array
-    except Exception as e:
-        st.error(f"Erreur de prétraitement : {e}")
-        return None
-# Fonction de prédiction
-def predict_user_image(image):
-    img_array = preprocess_image(image)
-    if img_array is None:
-        return "Erreur : Impossible de traiter l'image.", None, None
-    img_array = np.expand_dims(img_array, axis=0)
-    prediction = model.predict(img_array)
-    threshold = 0.487
-    probability = prediction[0][0] * 100
-    if probability >= threshold * 100:
-        return "Melanoma", probability, "red"
-    else:
-        return "Benign", (100 - probability), "green"
+
 # Interface Streamlit
 st.set_page_config(page_title="SkinCheck", layout="centered")
 # Charger le CSS
