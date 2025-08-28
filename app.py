3d22fcbcdd966a9a6dff6105a597645c0e82b442
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-08-28 02:24:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ elif st.session_state.screen == "Reframe":
 
         from streamlit_cropper import st_cropper
         cropped_image = st_cropper(image, realtime_update=True, box_color='#4A90E2', aspect_ratio=aspect_ratio)
-        st.image(cropped_image, caption="Frame the picture so that the mole takes half the space", use_container_width=True)
+        st.image(cropped_image, caption="Frame the picture so that the mole takes half the space", use_container_width=False, width=390)
         st.markdown(f'<div class="normal-text">Current size: {cropped_image.size[0]} x {cropped_image.size[1]}</div>', unsafe_allow_html=True)
         if st.button("Analyze", key="analyze"):
             with st.spinner("Analysis in progress..."):
