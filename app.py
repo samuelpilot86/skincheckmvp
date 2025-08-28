c1ab060859e9433fdfdced4481fe1e9eeee83b3e
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-08-28 12:48:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ else:
     reframed_mole_html = ""
 
 reframe_instructions_html = f'''
-    <table class="header-table">
+    <table class="instructions-table">
       <tr>
         <td><div class="normal-text">Move the frame to crop the picture so that the mole takes about half the space.</div></td>
         <td>{reframed_mole_html}</td>
