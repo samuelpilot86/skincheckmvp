42fcd3a44e24c3e0fb43335fd392d6418364a3a0
samuelpilot86 <samuel.pilot@mines-paris.org>
2025-09-14 23:44:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ if st.session_state.screen == "Accueil":
     # Réinitialiser l'historique à "Accueil" pour nettoyer les résidus
     st.session_state.screen_history = ["Accueil"]
     st.markdown(title_html, unsafe_allow_html=True)
-    st.markdown('<div class="normal-text">Submit a <b>clear, close-up* photo<b> of a concerning mole to our AI tool:</div>', unsafe_allow_html=True)
+    st.markdown('<div class="normal-text">Submit a <b>clear, close-up* photo</b> of a concerning mole to our AI tool:</div>', unsafe_allow_html=True)
     st.markdown('<div style="height:5px;"></div>', unsafe_allow_html=True) # Espacement réduit pour positionner plus haut
     # Solution CSS pour styliser les boutons et aligner verticalement
     st.markdown("""
